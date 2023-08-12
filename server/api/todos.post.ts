7aece1ced0344bbc40dde7ebb32cc11a244564b8
Amar Gill <amardeep@clearbanc.com>
2023-08-12 19:02:01 -0400

    change todo post handler to save to db

--- server/api/todos.post.ts
+++ server/api/todos.post.ts
@@ -1,8 +1,9 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  return {
-    content: body.content as string,
-    done: false,
-  };
+  return db
+    .insert(todos)
+    .values({ content: body.content, done: false })
+    .returning()
+    .get();
 });
