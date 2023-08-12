9a7cf5f408b8f6c2b3ad35cde3d59336f6036fbb
Amar Gill <amardeep@clearbanc.com>
2023-08-11 23:01:29 -0400

    render todos and add todos

--- app.vue
+++ app.vue
@@ -7,21 +7,34 @@
     <input type="text" name="content" />
     <button>Create</button>
   </form>
+  <ul>
+    <li v-for="todo in todos">{{ todo.content }} | {{ todo.done }}</li>
+  </ul>
 </template>
 
 <script setup lang="ts">
+type Todo = {
+  content: string;
+  done: boolean;
+};
+
 type FormPayload = Event & {
   currentTarget: EventTarget & HTMLFormElement;
 };
 
+const todos = ref<Todo[]>([
+  { content: "build app", done: false },
+  { content: "get rich", done: false },
+]);
+
 async function submitCreateTodo(payload: FormPayload) {
   const data = new FormData(payload.currentTarget);
 
-  const todo = await $fetch("/api/todos/create", {
+  const todo: Todo = await $fetch("/api/todos/create", {
     method: "POST",
     body: { content: data.get("content") },
   });
 
-  console.log(todo);
+  todos.value.push(todo);
 }
 </script>
