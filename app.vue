0aa1d8adfd4183939b38a3e6d37f3f881e231fef
Amar Gill <amardeep@clearbanc.com>
2023-08-11 22:08:23 -0400

    basic create todo form

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <form
+    method="post"
+    @submit.prevent="((e:FormPayload) => submitCreateTodo(e))"
+  >
+    <label for="content">Content</label>
+    <input type="text" name="content" />
+    <button>Create</button>
+  </form>
 </template>
+
+<script setup lang="ts">
+type FormPayload = Event & {
+  currentTarget: EventTarget & HTMLFormElement;
+};
+
+async function submitCreateTodo(payload: FormPayload) {
+  const data = new FormData(payload.currentTarget);
+
+  const todo = await $fetch("/api/todos/create", {
+    method: "POST",
+    body: { content: data.get("content") },
+  });
+
+  console.log(todo);
+}
+</script>
