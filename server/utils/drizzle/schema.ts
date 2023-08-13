1e94e2843cf403f9fa5b1135706497f497c24ab9
Amar Gill <amardeep@clearbanc.com>
2023-08-12 22:28:33 -0400

    set default value for done in schema

--- server/utils/drizzle/schema.ts
+++ server/utils/drizzle/schema.ts
@@ -4,7 +4,7 @@ import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
 export const todos = sqliteTable("todos", {
   id: integer("id").primaryKey(),
   content: text("content"),
-  done: integer("done", { mode: "boolean" }),
+  done: integer("done", { mode: "boolean" }).default(false),
 });
 
 export type Todo = InferModel<typeof todos>;
