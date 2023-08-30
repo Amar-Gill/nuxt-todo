ae1860fc9a13824c21acf7751dab8023fa27eff9
Amar Gill <amardeep@clearbanc.com>
2023-08-29 21:51:33 -0400

    delete drizzle dir

--- drizzle/0000_mysterious_sage.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `todos` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`content` text NOT NULL,
-	`done` integer DEFAULT false NOT NULL,
-	`created_at` integer DEFAULT (strftime('%s','now')) NOT NULL,
-	`updated_at` integer DEFAULT (strftime('%s','now')) NOT NULL
-);
