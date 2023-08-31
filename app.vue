27dcf52001fe5b021ad6d9127cf8d4934d56a620
Amar Gill <amardeep@clearbanc.com>
2023-08-30 20:04:07 -0400

    add navbar

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <template>
+  <nav>
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/todos">Todos</NuxtLink></li>
+    </ul>
+  </nav>
   <NuxtPage />
 </template>
 
