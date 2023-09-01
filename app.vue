c859e1be23328c098f0222d3451b8fdb289d9ca8
Amar Gill <amardeep@clearbanc.com>
2023-09-01 11:46:03 -0400

    conditionally render auth info in navbar

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <ul>
+      <li v-if="auth">Loggin in as: {{ auth?.user.userId }}</li>
       <li><NuxtLink to="/">Home</NuxtLink></li>
       <li><NuxtLink to="/todos">Todos</NuxtLink></li>
       <li><NuxtLink to="/login">Login</NuxtLink></li>
@@ -9,6 +10,9 @@
   <NuxtPage />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const authClient = useAuthClient();
+const auth = await authClient.getAuthenticationInfoOrNull();
+</script>
 
 <style></style>
