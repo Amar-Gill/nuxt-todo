c6793f48c51d87da3bcb2a884b3972e3ac8e498f
Amar Gill <amardeep@clearbanc.com>
2023-09-03 12:12:07 -0400

    use useAuth composable in middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,10 +3,11 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
   if (process.server) return;
 
-  const authClient = useAuthClient();
-  const auth = await authClient.getAuthenticationInfoOrNull();
+  const { fetchAuth } = useAuth();
 
-  if (!auth) {
+  const isLoggedIn = await fetchAuth();
+
+  if (!isLoggedIn) {
     if (to.path !== "/unauthorized") {
       return navigateTo("/unauthorized");
     }
