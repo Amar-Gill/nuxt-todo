f7fa766c8145b50a58b8390ad0b781311fd0d571
Amar Gill <amardeep@clearbanc.com>
2023-09-03 11:47:16 -0400

    useAuth composable

---
+++ composables/useAuth.ts
@@ -0,0 +1,23 @@
+import { AuthenticationInfo } from "@propelauth/javascript";
+
+export const useAuth = () => {
+  const authClient = useAuthClient();
+
+  const auth = ref<AuthenticationInfo | null>(null);
+  const fetchingAuth = ref(false);
+
+  const fetchAuth = async () => {
+    fetchingAuth.value = true;
+    try {
+      auth.value = await authClient.getAuthenticationInfoOrNull();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      fetchingAuth.value = false;
+    }
+  };
+
+  if (!auth.value) fetchAuth();
+
+  return { auth, authClient, fetchingAuth };
+};
