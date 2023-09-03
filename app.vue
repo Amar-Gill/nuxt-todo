cd5d93fa483f3344e11840088f7e112a89d1b45c
Amar Gill <amardeep@clearbanc.com>
2023-09-03 11:47:35 -0400

    handle auth loading state in app.vue

--- app.vue
+++ app.vue
@@ -1,18 +1,19 @@
 <template>
   <nav>
     <ul>
+      <li v-if="fetchingAuth">Loading...</li>
       <li v-if="auth">Loggin in as: {{ auth?.user.userId }}</li>
       <li><NuxtLink to="/">Home</NuxtLink></li>
       <li><NuxtLink to="/todos">Todos</NuxtLink></li>
       <li><NuxtLink to="/login">Login</NuxtLink></li>
     </ul>
   </nav>
-  <NuxtPage />
+  <div v-if="fetchingAuth">Loading...</div>
+  <NuxtPage v-else />
 </template>
 
 <script setup lang="ts">
-const authClient = useAuthClient();
-const auth = await authClient.getAuthenticationInfoOrNull();
+const { auth, fetchingAuth } = useAuth();
 </script>
 
 <style></style>
