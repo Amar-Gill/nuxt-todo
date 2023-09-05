bb615cf89a045eff40215961c9f06a0f203f872c
Amar Gill <amardeep@clearbanc.com>
2023-09-05 01:05:01 -0400

    clean up nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,12 @@ import { drizzle } from "drizzle-orm/libsql";
 import { migrate } from "drizzle-orm/libsql/migrator";
 import { createClient } from "@libsql/client";
 
-const tursoDbUrl = "file:./server/sqlite.db";
-const tursoDbToken = "";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
-    tursoDbUrl,
-    tursoDbToken,
+    tursoDbUrl: "file:./server/sqlite.db",
+    tursoDbToken: "",
     public: {
       authUrl: "",
     },
@@ -26,14 +23,14 @@ export default defineNuxtConfig({
       console.info("Migrating database...");
 
       const client = createClient({
-        url: process.env.NUXT_TURSO_DB_URL ?? tursoDbUrl,
-        authToken: process.env.NUXT_TURSO_DB_TOKEN ?? tursoDbToken,
+        url: process.env.NUXT_TURSO_DB_URL as string,
+        authToken: process.env.NUXT_TURSO_DB_TOKEN,
       });
 
       const db = drizzle(client);
 
       await migrate(db, { migrationsFolder: "./server/migrations" })
-        .then(() => console.log("Database migrated"))
+        .then(() => console.info("Database migrated"))
         .catch(console.error);
     },
   },
