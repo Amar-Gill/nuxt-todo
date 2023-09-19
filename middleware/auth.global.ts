1c467c873303951c5eb470b4778aa1479b669551
Amar Gill <amardeep@clearbanc.com>
2023-09-18 22:10:47 -0700

    remove unused arg

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,4 +1,4 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   if (to.path === '/login') return;
 
   if (process.server) return;
