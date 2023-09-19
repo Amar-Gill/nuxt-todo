1c66539102d9a947cbd74711b5fff57b4e317dff
Amar Gill <amardeep@clearbanc.com>
2023-09-19 00:32:03 -0700

    lint .eslintrc file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config"],
+  extends: ['@nuxt/eslint-config'],
   rules: {
     // Global
-    quotes: ["error", "single"],
-    "quote-props": ["error", "as-needed"],
-    "comma-dangle": [
-      "error",
+    quotes: ['error', 'single'],
+    'quote-props': ['error', 'as-needed'],
+    'comma-dangle': [
+      'error',
       {
-        arrays: "never",
-        objects: "always-multiline",
-        imports: "never",
-        exports: "never",
-        functions: "never",
-      },
+        arrays: 'never',
+        objects: 'always-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      }
     ],
   },
 };
