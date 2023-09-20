846c347483191c8253324a9665fe57be0260ddd3
Amar Gill <amardeep@clearbanc.com>
2023-09-19 18:52:05 -0700

    set up blog pages

---
+++ pages/users/[id]/blog/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+});
+</script>
+
+<template>
+  <div>Page: /users/:userId/blog</div>
+</template>
+
+<style scoped></style>
---
+++ pages/users/[id]/blog/new.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+});
+</script>
+
+<template>
+  <div>Page: /users/:userId/blog/new</div>
+</template>
+
+<style scoped></style>
