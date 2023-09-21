f9b7b43633dd54782f93681aebe3e33a931e0801
Amar Gill <amardeep@clearbanc.com>
2023-09-20 21:43:45 -0700

    save btn logs json content

--- pages/users/[id]/blog/new.vue
+++ pages/users/[id]/blog/new.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+import TipTap from '~/components/TipTap.vue';
+
 definePageMeta({
   middleware: 'auth',
 });
+
+const tiptap = ref<InstanceType<typeof TipTap> | null>(null);
+
+const save = () => {
+  const json = tiptap.value?.json;
+  console.log('editor json', json);
+};
 </script>
 
 <template>
-  <div><TipTap /></div>
+  <div>
+    <div>
+      <button @click="save">
+        Save~~~
+      </button>
+    </div>
+    <TipTap ref="tiptap" />
+  </div>
 </template>
 
 <style scoped></style>
