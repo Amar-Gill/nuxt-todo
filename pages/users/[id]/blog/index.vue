2ca8cb602334233895df8b1acc51711058921552
Amar Gill <amardeep@clearbanc.com>
2023-09-21 18:29:56 -0700

    render count of blog posts on page

--- pages/users/[id]/blog/index.vue
+++ pages/users/[id]/blog/index.vue
@@ -2,10 +2,25 @@
 definePageMeta({
   middleware: 'auth',
 });
+
+const { auth, fetchingAuth } = useAuth();
+
+const { data, pending, error } = await useFetch(
+  `/api/posts?author-id=${auth.value?.user.userId}`,
+  { headers: { Authorization: `Bearer ${auth.value?.accessToken}` } }
+);
 </script>
 
 <template>
-  <div>Page: /users/:userId/blog</div>
+  <div v-if="fetchingAuth || (pending && !data && !error)">
+    Loading... 🚰
+  </div>
+  <div v-else-if="error">
+    {{ error }}
+  </div>
+  <div v-else>
+    Num posts: {{ data?.posts.length }}
+  </div>
 </template>
 
 <style scoped></style>
