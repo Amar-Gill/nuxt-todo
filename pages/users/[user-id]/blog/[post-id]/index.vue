4ef05812e4a512c49e97166ac5084abf81918cb1
Amar Gill <amardeep@clearbanc.com>
2023-09-24 15:05:46 -0700

    render post html content on blog post page

--- pages/users/[user-id]/blog/[post-id]/index.vue
+++ pages/users/[user-id]/blog/[post-id]/index.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+import Document from '@tiptap/extension-document';
+import Paragraph from '@tiptap/extension-paragraph';
+import Text from '@tiptap/extension-text';
+import Heading from '@tiptap/extension-heading';
+import Blockquote from '@tiptap/extension-blockquote';
+import BulletList from '@tiptap/extension-bullet-list';
+import CodeBlock from '@tiptap/extension-code-block';
+import ListItem from '@tiptap/extension-list-item';
+import Italic from '@tiptap/extension-italic';
+import Bold from '@tiptap/extension-bold';
+import HardBreak from '@tiptap/extension-hard-break';
+import { generateHTML } from '@tiptap/html';
+
 definePageMeta({
   middleware: 'auth',
 });
@@ -13,6 +26,24 @@ const { data, pending, error } = await useFetch(
     headers: { Authorization: `Bearer ${auth.value?.accessToken}` },
   }
 );
+
+const tiptapExtensions = [
+  Document,
+  Paragraph,
+  Text,
+  Heading,
+  Blockquote,
+  BulletList,
+  CodeBlock,
+  ListItem,
+  Italic,
+  Bold,
+  HardBreak,
+];
+
+const htmlString = computed(() =>
+  generateHTML(data.value.post.content, tiptapExtensions)
+);
 </script>
 
 <template>
@@ -24,5 +55,6 @@ const { data, pending, error } = await useFetch(
   </div>
   <div v-else>
     a blog id: {{ data?.post.id }}
+    <div v-html="htmlString" />
   </div>
 </template>
