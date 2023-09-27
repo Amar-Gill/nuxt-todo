fa60e293c1ea100a68a7e2a45a8967ee612c82b0
Amar Gill <amardeep@clearbanc.com>
2023-09-26 22:32:22 -0400

    render editor on /edit page with correct content

--- pages/users/[user-id]/blog/[post-id]/edit.vue
+++ pages/users/[user-id]/blog/[post-id]/edit.vue
@@ -1,7 +1,22 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { params } = useRoute();
+
+const { auth, fetchingAuth } = useAuth();
+
+const { data, pending, error } = await useFetch(
+  `/api/posts/${params.postid.toString()}`,
+  { headers: { Authorization: `Bearer ${auth.value?.accessToken}` } }
+);
+</script>
 
 <template>
-  <div>Page: edit le post</div>
+  <div>
+    omg le edit!!!!!!!!!!!!
+    <TipTap
+      ref="tiptap"
+      :content="data.post.content"
+    />
+  </div>
 </template>
 
 <style scoped></style>
