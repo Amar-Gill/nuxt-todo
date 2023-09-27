8cf1f85f4f9e5495a9286d3aecede485523b232c
Amar Gill <amardeep@clearbanc.com>
2023-09-27 01:02:23 -0400

    save btn on edit blog post page

--- pages/users/[user-id]/blog/[post-id]/edit.vue
+++ pages/users/[user-id]/blog/[post-id]/edit.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import TipTap from '~/components/TipTap.vue';
+import { InsertBlogPostSansAuth } from '~/server/utils/drizzle/schema';
+
 const { params } = useRoute();
 
 const { auth, fetchingAuth } = useAuth();
@@ -7,11 +10,43 @@ const { data, pending, error } = await useFetch(
   `/api/posts/${params.postid.toString()}`,
   { headers: { Authorization: `Bearer ${auth.value?.accessToken}` } }
 );
+
+const tiptap = ref<InstanceType<typeof TipTap> | null>(null);
+
+const saving = ref(false);
+
+const save = async () => {
+  saving.value = true;
+
+  const json = tiptap.value?.editor?.getJSON();
+
+  const patchProps = {
+    title: 'updated title',
+    content: json,
+  } satisfies InsertBlogPostSansAuth;
+
+  $fetch(`/api/posts/${params.postid.toString()}`, {
+    method: 'PATCH',
+    body: patchProps,
+    headers: { Authorization: `Bearer ${auth.value?.accessToken}` },
+  })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(() => {
+      saving.value = false;
+    });
+};
 </script>
 
 <template>
   <div>
-    omg le edit!!!!!!!!!!!!
+    <button
+      :disabled="saving"
+      @click="save"
+    >
+      Omg le Save!
+    </button>
     <TipTap
       ref="tiptap"
       :content="data.post.content"
