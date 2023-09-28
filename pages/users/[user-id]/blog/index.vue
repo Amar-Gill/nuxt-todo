c67f0b50d72961a9ae7f29a80e8d77121905b05a
Amar Gill <amardeep@clearbanc.com>
2023-09-27 23:59:21 -0400

    fix type error on blog/index

--- pages/users/[user-id]/blog/index.vue
+++ pages/users/[user-id]/blog/index.vue
@@ -24,14 +24,14 @@ const { data, pending, error } = await useFetch(
     Num posts: {{ data?.posts.length }}
     <ul>
       <li
-        v-for="post in data.posts"
+        v-for="post in data?.posts"
         :key="post.id"
       >
-        <NuxtLink :to="`/users/${auth.user.userId}/blog/${post.id}`">
+        <NuxtLink :to="`/users/${auth?.user.userId}/blog/${post.id}`">
           {{ post.title }}
         </NuxtLink>
         |
-        <NuxtLink :to="`/users/${auth.user.userId}/blog/${post.id}/edit`">
+        <NuxtLink :to="`/users/${auth?.user.userId}/blog/${post.id}/edit`">
           Edit ✎
         </NuxtLink>
       </li>
